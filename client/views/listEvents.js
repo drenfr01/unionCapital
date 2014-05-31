49e080a322447026e2cf2befdb344cc185b782f7
drenfr01 <duncanrenfrow@gmail.com>
2014-05-31 15:03:25 -0400

    events modal working with data context

--- client/views/listEvents.js
+++ client/views/listEvents.js
@@ -1,5 +1,19 @@
+Template.listEvents.rendered = function() {
+  //Session.set('modalDataContext', n);
+};
+
 Template.listEvents.helpers({
-  'events': function() {
+  'communityEvents': function() {
     return Events.find({active: 1}, {sort: {startDate: 1}});
+  },
+  'modalContext': function() {
+    return Session.get('modalDataContext');
+  }
+});
+
+Template.listEvents.events({
+  'click #editEvent': function(e) {
+    console.log(this);
+    Session.set('modalDataContext', this);
   }
 });
