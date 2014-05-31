1403f2e8d18f4edef1ac1b464f46887682e70445
Jing <jingniwei@gmail.com>
2014-05-31 17:28:43 -0400

    made event list clickable and display event map

--- client/views/listEvents.js
+++ client/views/listEvents.js
@@ -1,5 +1,27 @@
+
 Template.listEvents.helpers({
-  'events': function() {
-    return Events.find({active: 1}, {sort: {startDate: 1}});
-  }
+  'communityEvents': function() {
+  		return Events.find({active: 1}, {sort: {startDate: 1}});
+  },
+  'isEventIndex': function() {
+  		return Session.get('eventIndex');
+  },
+  'eventView': function() {
+  		return Session.get('event');
+	}
 });
+
+Template.listEvents.events({
+	'click .eventView': function(e) {
+		Session.set('eventIndex', false);
+		Session.set('event', this);
+	}, 
+	'click .back': function(e) {
+		Session.set('eventIndex', true);
+		Session.set('event', null);
+	}
+});
+
+Template.listEvents.rendered = function() {	
+	Session.set('eventIndex', true);
+};
