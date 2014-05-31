da74bdd322b55abf329ae5a21262e2039ee6e638
drenfr01 <duncanrenfrow@gmail.com>
2014-05-31 09:11:49 -0400

    getting mock up of scanning QR code to work

--- client/views/listEvents.js
+++ client/views/listEvents.js
@@ -1,8 +1,5 @@
 Template.listEvents.helpers({
   'events': function() {
     return Events.find({active: 1}, {sort: {startDate: 1}});
-  },
-  'formattedDate': function(unformattedDate) {
-    return moment(unformattedDate).format('MMMM DD YYYY');
   }
 });
