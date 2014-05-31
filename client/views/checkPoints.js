a626d8767561e16dcd903a96be0a7d20363c76cf
Isaac Chansky <ichansky@gmail.com>
2014-05-31 16:11:19 -0400

    add helpers to get pull in event attributes

--- client/views/checkPoints.js
+++ client/views/checkPoints.js
@@ -1,8 +1,24 @@
+function getEvent(transaction){
+  var id = transaction.eventID;
+  var event = Events.findOne({ _id: id });
+  return event;
+}
+
 Template.checkPoints.helpers({
   'totalPoints': function() {
-    return Transactions.find({userId: Meteor.userId()}).fetch().reduce(function(sum, transaction) {
-      return sum += transaction.points;
-    }, 0);
+    return Transactions
+      .find({userId: Meteor.userId()})
+      .fetch()
+      .reduce(function(sum, transaction) {
+        var event = getEvent(transaction);
+        return sum += event.points;
+      }, 0);
+  },
+  'getEvent': function(){
+    return getEvent(this).name;
+  },
+  'getPoints': function(){
+    return getEvent(this).points;
   },
   'activities': function() {
     return Transactions.find(
