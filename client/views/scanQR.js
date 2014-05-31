5b54ef109b6cdd2f20298106679f2a95af979343
Isaac Chansky <ichansky@gmail.com>
2014-05-31 14:20:15 -0400

    remove points from transactions

--- client/views/scanQR.js
+++ client/views/scanQR.js
@@ -8,29 +8,15 @@ UI.registerHelper("activityOptions", function() {
   return options;
 });
 
-Template.scanQR.events({
-  'submit': function(e) {
-    var attributes = {
-      points: parseInt($('#userPoints').val(), 10),
-      userId:  Meteor.userId()
-    };
-
-    Meteor.call('updateUserPoints', attributes, function(error,response) {
-      if(error) {
-        throwError(error.reason, 'alert-danger');
-        Router.go('scanQR');
-      }
-      throwError('Points Added!', 'alert-success');
-      Router.go('memberHomePage');
-    });
-  }
-});
-
 AutoForm.addHooks('insertTransactionForm', {
   before: {
     insert: function(transaction) {
       transaction.userId = Meteor.userId();
       return transaction;
     }
+  },
+  onSuccess: function(){
+    throwError('Points Added!', 'alert-success');
+    Router.go('memberHomePage');
   }
 });
