f0761c67ff701dbac8a82be5aba7cd7f39d5d7f4
Liam Morley <liam@carpeliam.com>
2014-05-31 12:45:09 -0400

    calculate points based on associated transactions

--- client/views/checkPoints.js
+++ client/views/checkPoints.js
@@ -1,6 +1,8 @@
 Template.checkPoints.helpers({
   'totalPoints': function() {
-    return Meteor.user().profile.points;
+    return Transactions.find({userId: Meteor.userId()}).fetch().reduce(function(sum, transaction) {
+      return sum += transaction.points;
+    }, 0);
   },
   'activities': function() {
     //TODO: currently pulling all transactions, need to limit it by userId. This involves setting a hidden field for userId on autoform?
