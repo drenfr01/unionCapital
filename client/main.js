31b9effe3e2983c747d2231d772a55e8f0398ae8
Isaac Chansky <ichansky@gmail.com>
2014-06-01 14:36:46 -0400

    publish and subscribe to collection

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ Meteor.subscribe('events');
 Meteor.subscribe('transactions');
 Meteor.subscribe('images');
 Meteor.subscribe('userData');
+Meteor.subscribe('rewards');
 
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_AND_EMAIL'
