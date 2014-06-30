e6e77ed52a0039c4cabdda046fbef424eaba85cd
drenfr01 <duncanrenfrow@gmail.com>
2014-06-29 19:01:14 -0500

    merging landing, login, and create user pages into one template

--- client/views/landing.js
+++ client/views/landing.js
@@ -1,5 +1,5 @@
 Template.landing.rendered = function() {
-  Session.set('loginStateVar', 'landingPage');
+  Session.set('loginStateVar', 'loginPage');
 };
 
 Template.landing.helpers({
@@ -9,13 +9,53 @@ Template.landing.helpers({
 });
 
 Template.landing.events({
-  'click #login': function(e) {
+  'click #signIn': function(e) {
     e.preventDefault();
     Session.set('loginStateVar', 'loginPage');
   },
   'click #signUp': function(e) {
     e.preventDefault();
     Session.set('loginStateVar', 'signUp');
+  },
+  'click #loginSubmit': function(e) {
+    e.preventDefault();
+
+    var email =  $('#userEmail').val();
+    var password = $('#userPassword').val();
+
+    Meteor.loginWithPassword(email, password, function(error) {
+      if(error) {
+        addErrorMessage(error.reason);
+        Router.go('loginPage');
+      } else {
+        if(Roles.userIsInRole(Meteor.userId(), 'admin')) {
+          Router.go('adminHomePage');
+        } else {
+          Router.go('memberHomePage');
+        }
+      }
+    });
+  },
+  'click #createNewUser': function(e) {
+    e.preventDefault();
+
+    Accounts.createUser({
+      email: $('#userEmail').val(),
+      password: $('#userPassword').val(),
+      profile: {
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        street: $('#userStreet').val(),
+        city: $('#userCity').val(),
+        state: $('#userState').val()
+      }
+    }, function(error) {
+      if(error) {
+        addErrorMessage(error.reason);
+      }
+      addSuccessMessage("Successfully Created User");
+      Router.go('memberHomePage');
+    });
   }
 });
 
