66112ec972dd9417ee4de906c7cf0442fa1d5e8e
drenfr01 <duncanrenfrow@gmail.com>
2014-07-12 17:04:34 -0400

    fixed bug with incorrect password message for all logins

--- client/views/landing.js
+++ client/views/landing.js
@@ -63,27 +63,3 @@ Template.landing.events({
 
   }
 });
-
-
-Template.landing.events({
-  'click #loginSubmit': function(e) {
-    e.preventDefault();
-
-    var email =  $('#userEmail').val();
-    var password = $('#userPassword').val();
-
-    Meteor.loginWithPassword(email, password, function(error) {
-      if(error) {
-        addErrorMessage(error.reason);
-        /*Router.go('landing');*/
-      } else {
-        if(Roles.userIsInRole(Meteor.userId(), 'admin')) {
-          Router.go('adminHomePage');
-        } else {
-          Router.go('memberHomePage');
-        }
-      }
-    });
-
-  }
-});
