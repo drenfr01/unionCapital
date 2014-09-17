2308a316f482437ce24c768eb2b8038d7538d921
drenfr01 <duncanrenfrow@gmail.com>
2014-09-17 13:47:40 -0400

    removing dead fromItem code

--- client/views/formItem.js
+++
@@ -1,9 +0,0 @@
-Template.formItem.customerMeasurement = function(item) {
-  customerId = Session.get('currentCustomer');
-
-  customerMeasurements = Customers.findOne({_id: customerId}).measurements;
-  if(!_.isUndefined(customerMeasurements)) {
-    return 'Currently: ' + customerMeasurements[item]
-  }
-  return 'No Measurement Taken Yet'
-}
