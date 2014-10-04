4119beeba621250fc8921acddda50b7698c9f653
drenfr01 <duncanrenfrow@gmail.com>
2014-10-04 16:38:14 -0400

    centering events map on user

--- client/views/communityNeeds.js
+++ client/views/communityNeeds.js
@@ -1,52 +1,70 @@
 var map;
 
 Template.communityNeeds.rendered = function() {
-  var mapOptions = {
-    center: new google.maps.LatLng(42.3581, -71.0636),
-    zoom: 13
+
+  var geoOptions = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
   };
-  map = new google.maps.Map(document.getElementById("map-canvas"),
-    mapOptions);
 
-    //set start of week date
-    var currentDate = new Date();
-    currentDate.setHours(0,0,0,0);
-    var startDayDate = new Date(currentDate);
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
 
-    //set end of week date
-    currentDate = new Date();
-    currentDate.setHours(23,59,59,59);
-    var endDayDate = new Date(currentDate);
+      var mapOptions = {
+        center: new google.maps.LatLng(position.coords.latitude, 
+                                       position.coords.longitude),
+        zoom: 13
+      };
+      map = new google.maps.Map(document.getElementById("map-canvas"),
+        mapOptions);
 
-    activeEvents = Events.find({startDate: {'$lte': endDayDate}, 
-                     endDate: {'$gte': startDayDate}, 
-                     active: 1},
-                     {sort: {startDate: 1}});
- 
-  var geocoder = new google.maps.Geocoder();
+        //set start of week date
+        var currentDate = new Date();
+        currentDate.setHours(0,0,0,0);
+        var startDayDate = new Date(currentDate);
 
-  activeEvents.forEach(function (place) {
+        //set end of week date
+        currentDate = new Date();
+        currentDate.setHours(23,59,59,59);
+        var endDayDate = new Date(currentDate);
 
-    geocoder.geocode( { 'address': place.address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        var infowindow = new google.maps.InfoWindow({
-          content: "Name: " + place.name
-        });
+        activeEvents = Events.find({startDate: {'$lte': endDayDate}, 
+                         endDate: {'$gte': startDayDate}, 
+                         active: 1},
+                         {sort: {startDate: 1}});
+     
+      var geocoder = new google.maps.Geocoder();
 
-        var marker = new google.maps.Marker({
-          position: results[0].geometry.location ,
-          map: map,
-          title: place.name
-        });
+      activeEvents.forEach(function (place) {
 
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.open(map,marker);
-        });
-      } else {
-        addErrorMEssage('Geocode was not successful for the following reason ' +
-          status);
-      }
+        geocoder.geocode( { 'address': place.address}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            var infowindow = new google.maps.InfoWindow({
+              content: "Name: " + place.name
+            });
+
+            var marker = new google.maps.Marker({
+              position: results[0].geometry.location ,
+              map: map,
+              title: place.name
+            });
+
+            google.maps.event.addListener(marker, 'click', function() {
+              infowindow.open(map,marker);
+            });
+        } else {
+          addErrorMEssage('Geocode was not successful for the following reason ' +
+            status);
+        }
+      });
     });
-  });
+
+    }, function(error) {
+      addErrorMessage(error.message);
+    }, geoOptions);
+  } else {
+    addErrorMessage('Geolocation not supported, could not center map');
+  }
 };
 
