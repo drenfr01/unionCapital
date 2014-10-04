26d30b985b46a6335d1d1b4b5b6ff4212263764a
drenfr01 <duncanrenfrow@gmail.com>
2014-10-04 16:18:15 -0400

    members can now open a map of all current or future events

--- client/views/communityNeeds.js
+++ client/views/communityNeeds.js
@@ -8,17 +8,28 @@ Template.communityNeeds.rendered = function() {
   map = new google.maps.Map(document.getElementById("map-canvas"),
     mapOptions);
 
-  activeEvents = Events.find({active: 1});
+    activeEvents = Events.find({active: 1, 
+                               endDate: {'$gte': new Date()}}, 
+                               {limit: 10}
+                              );
   var geocoder = new google.maps.Geocoder();
 
   activeEvents.forEach(function (place) {
 
     geocoder.geocode( { 'address': place.address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
+        var infowindow = new google.maps.InfoWindow({
+          content: "Name: " + place.name
+        });
+
         var marker = new google.maps.Marker({
           position: results[0].geometry.location ,
           map: map,
-          title: place.description
+          title: place.name
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.open(map,marker);
         });
       } else {
         addErrorMEssage('Geocode was not successful for the following reason ' +
