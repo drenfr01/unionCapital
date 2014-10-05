932fa8de7b42392e9e0b465e5c84a6c98d26d181
drenfr01 <duncanrenfrow@gmail.com>
2014-10-05 18:28:04 -0400

    cleaning up facebook plugin, but still doesn't re-render...

--- client/views/memberHomePage.js
+++ client/views/memberHomePage.js
@@ -1,3 +1,16 @@
+Template.memberHomePage.rendered = function() {
+  var d = document;
+  var s = 'script';
+  var id = 'facebook-jssdk';
+      
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); 
+  js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1518343331716121&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+};
+
 Template.memberHomePage.helpers({
   'currentMemberName': function() {
     if(Meteor.user()) {
