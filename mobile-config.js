fdb851d6c82a9baa771d14a20eb326fda1ef18b8
drenfr01 <duncanrenfrow@gmail.com>
2014-10-15 21:03:07 -0400

    upgrade to 0.9.4

---
+++ mobile-config.js
@@ -0,0 +1,9 @@
+App.info({
+  name: 'Union Capital',
+  description: 'A mobile application to empower community members',
+  author: 'Union Capital Dev Team',
+  email: 'eric.leslie@unioncapitalboston.com',
+  website: 'unioncapitalboston.com'
+});
+
+//TODO: Set launch screens, icons, etc.
