a288d6e357d418513d32592a9d0ee827c2f85607
drenfr01 <duncanrenfrow@gmail.com>
2015-02-22 21:30:40 -0500

    refactored layout into separate navbars

--- meteor/client/common/layout.js
+++ meteor/client/common/layout.js
@@ -1,12 +1,3 @@
 Template.layout.events({
-  'click #signOut': function(e) {
-    Meteor.logout(function(error) {
-      if(error) {
-        addErrorMessage(error.reason);
-      } else {
-        Router.go('landing');
-      }
-    });
-  }
 });
 
