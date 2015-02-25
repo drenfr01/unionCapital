aba9bbea9493276c383e073ba495fcd89e2e3269
drenfr01 <duncanrenfrow@gmail.com>
2015-02-24 23:36:13 -0500

    close to getting super admin member search working

--- meteor/client/admin/allMembers.js
+++ meteor/client/admin/allMembers.js
@@ -7,13 +7,29 @@ var fields = ['profile.firstName', 'profile.lastName'];
 
 MemberNameSearch = new SearchSource('memberSearch', fields, options);
 
+Session.set('sortOrder', -1);
+Session.set('sortOn', 'totalPoints');
+
+var highlightSortedColumn = function(target) {
+    $(".tableSort").css('color', 'black');
+    $(target).css('color','red');
+};
+
+Template.allMembers.rendered = function() {
+  MemberNameSearch.search("");
+  highlightSortedColumn("#" + Session.get('sortOn'));
+};
+
 Template.allMembers.helpers({
   getMembers: function() {
+    var sortOn = function() {return Session.get("sortOn"); };
+    var sortOrder = function() {return Session.get("sortOrder"); };
     return MemberNameSearch.getData({
       transform: function(matchText, regExp) {
+        console.log(sortOn);
         return matchText.replace(regExp, "<b>$&</b>");
       },
-      sort: {"profile.firstName": -1}
+      sort: {sortOn: sortOrder}
     });
   }
 });
@@ -22,5 +38,36 @@ Template.allMembers.events({
   "keyup #search-box": _.throttle(function(e) {
     var text = $(e.target).val().trim();
     MemberNameSearch.search(text);
-  }, 200)
+  }, 200),
+  'click #firstName': function(e) {
+    Session.set('sortOn', 'firstName');
+    highlightSortedColumn(e.target);
+  },
+  'click #zip': function(e) {
+    Session.set('sortOn', 'zip');
+    highlightSortedColumn(e.target);
+  },
+  'click #transactions': function(e) {
+    Session.set('sortOn', 'numberOfTransactions');
+    highlightSortedColumn(e.target);
+  },
+  'click #totalPoints': function(e) {
+    Session.set('sortOn', 'totalPoints');
+    highlightSortedColumn(e.target);
+  },
+  'click #lastEvent': function(e) {
+    Session.set('sortOn', 'lastEvent');
+    highlightSortedColumn(e.target);
+  },
+  'click #lastEventDate': function(e) {
+    Session.set('sortOn', 'lastEventDate');
+    highlightSortedColumn(e.target);
+  },
+  'change .radio-inline': function(e) {
+    if(e.target.value === "ascending") {
+      Session.set('sortOrder',1);
+    } else {
+      Session.set('sortOrder', -1);
+    }
+  }
 });
--- meteor/server/memberSearch.js
+++ meteor/server/memberSearch.js
@@ -1,25 +1,56 @@
 SearchSource.defineSource('memberSearch', function(searchText, options) {
   try {
-    //TODO: why do we overwrite options here? it's a parameter above, but
-    //yet this is the way it's done in the docs...
-    //I think this is done because you there is both 
-    //a server side and client side def of this method, 
-    //so it either calls the client side or after the timeout
+    //I think we overwrite options because there is both a server side and client side def 
+    //of this method, so it either calls the client side or after the timeout
     //does the server side 
     var options = {
       sort: {"profile.firstName": 1},
       limit: 20
     };
-    if(searchText) {
+    var users = [];
+
+    if(searchText && searchText.length > 0) {
       var regExp = buildRegExp(searchText);
       var selector = {$or: [
         {"profile.firstName": regExp},
         {"profile.lastName": regExp}
       ]};
-      return Meteor.users.find(selector, options).fetch();
+      users = Meteor.users.find(selector, options).fetch();
     } else {
-      return [];
+      users = Meteor.users.find({}, options).fetch();
     }
+    
+      //TODO: THE BELOW CODE SNIPPET IS AN OFFENSE TO GOD AND MEN
+      var tableRows = _.map(users, function(user) {
+      
+      //WARNING: unclear if below is a big performance hit (2 cursor calls)
+      var transactionCount = Transactions.find({userId: user._id}).count();
+      var totalPoints = Meteor.users.totalPointsFor(user._id);
+      var mostRecentTransaction = Transactions.find({userId: user._id}, 
+                            {sort: {transactionDate: -1}, limit: 1}).fetch()[0] ||
+                              { eventId: "", transactionDate: ""};
+      var mostRecentEvent = Events.findOne(mostRecentTransaction.eventId) || {name: ""};
+      
+      //if user is admin
+      var userProfile = user.profile || {firstName: 'admin', lastName: '', zip: ''};
+      //if user is logging in with facebook
+      var userFirstName = userProfile.firstName || userProfile.name || "";
+      var userLastName = userProfile.lastName || userProfile.name || "";
+      var userZip = userProfile.zip || "";
+
+
+
+      return {
+        firstName: userFirstName.toLowerCase(),
+        lastName: userLastName.toLowerCase(), 
+        zip: userZip,
+        lastEvent: mostRecentEvent.name,
+        lastEventDate: mostRecentTransaction.transactionDate,
+        numberOfTransactions: transactionCount, 
+        totalPoints: totalPoints};
+    });
+
+    return tableRows;
   } catch(e) {
     console.log(e.reason);
   }
