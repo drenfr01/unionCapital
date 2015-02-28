b8c972526b10d3b911c1391889253b590b942670
Drew Proud <drew.proud@hotmail.com>
2015-02-28 23:22:46 +0000

    Added vagrant setup files

---
+++ vagrant/simlink.sh
@@ -0,0 +1,2 @@
+﻿sudo mount --bind /home/vagrant/unionCapital/meteor/.meteor/ /vagrant/unionCapital/meteor/.meteor/
+echo “sudo mount --bind /home/vagrant/unionCapital/meteor/.meteor/ /vagrant/unionCapital/meteor/.meteor/” >> ~/.bashrc && source ~/.bashrc
