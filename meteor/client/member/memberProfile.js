255a34b4575e7aa8a7e9e5c087944c8e728b7a6f
drenfr01 <duncanrenfrow@gmail.com>
2015-03-02 18:07:26 -0500

    adding in packages and csv upload screen

--- meteor/client/member/memberProfile.js
+++ meteor/client/member/memberProfile.js
@@ -1,5 +1,21 @@
 Template.memberProfile.helpers({
   memberProfile: function() {
     return Meteor.user().profile;
+  },
+  memberPhoto: function() {
+    //TODO: this is insecure, need to set the access functions
+    return MemberProfilePhotos.find();
+  }
+});
+
+Template.memberProfile.events({
+  'change #profilePhoto': function(event, template) {
+    FS.Utility.eachFile(event, function(file) {
+      var newFile = new FS.File(file);
+      newFile.metadata = {userId: Meteor.userId(), timestamp: moment().toDate()};
+      MemberProfilePhotos.insert(newFile, function(err, fileObj) {
+
+      });
+    });
   }
 });
--- meteor/collections/images.js
+++ meteor/collections/images.js
@@ -18,3 +18,27 @@ Images.allow({
     return true;
   }
 });
+
+var memberPhotoStore = new FS.Store.FileSystem("memberProfilePhotos", {
+  maxTries: 1
+});
+
+
+MemberProfilePhotos = new FS.Collection("memberProfilePhotos", {
+  stores: [memberPhotoStore]
+});
+
+MemberProfilePhotos.allow({
+  insert: function(userId, doc) {
+    return true;
+  },
+  update: function(userId, doc, fieldNames, modifier) {
+    return true;
+  },
+  remove: function(userId, doc) {
+    return true;
+  },
+  download: function(userId, doc) {
+    return true;
+  }
+});
