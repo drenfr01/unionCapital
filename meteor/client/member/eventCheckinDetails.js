02ae06eafad1a4efa5ed890c5856a809236961e6
Drew Proud <drew.proud@hotmail.com>
2015-03-14 00:12:16 +0000

    Added a slider to pick time attended

--- meteor/client/member/eventCheckinDetails.js
+++ meteor/client/member/eventCheckinDetails.js
@@ -1,3 +1,38 @@
+
+var defaultHours = 3
+hours = new ReactiveVar(defaultHours);
+
 Template.eventCheckinDetails.rendered = function() {
-	
+	$('#durationSlider').noUiSlider({
+		start: [defaultHours],
+		range: {
+			min: [0],
+			max: [8]
+		},
+		step: 0.5,
+		format: wNumb({
+			decimals: 1
+		})
+	});
+
+	$('#durationSlider').noUiSlider_pips({
+		mode: 'values',
+		values: [0,1,2,3,4,5,6,7,8]
+		// filter: function(value, type) {
+		// 	return value%1 ? 1 : 0;
+		// }
+	});
 }
+
+Template.eventCheckinDetails.helpers({
+
+	'timeAttending': function() {
+		return hours.get();
+	}
+})
+
+Template.eventCheckinDetails.events({
+	'change #durationSlider': function() {
+		hours.set($('#durationSlider').val());
+	}
+})
