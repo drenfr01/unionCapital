4e7b3f1da160c21dfa2343250e7f8a5ca77dcecc
Tyler Sheffels <tsheffels@dimagi.com>
2015-03-15 19:04:46 -0400

    Refactor the member home page to remove handling on links

--- meteor/client/member/memberHomePage.js
+++ meteor/client/member/memberHomePage.js
@@ -2,10 +2,10 @@ Template.memberHomePage.rendered = function() {
   var d = document;
   var s = 'script';
   var id = 'facebook-jssdk';
-      
+
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
-  js = d.createElement(s); 
+  js = d.createElement(s);
   js.id = id;
   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1518343331716121&version=v2.0";
   fjs.parentNode.insertBefore(js, fjs);
@@ -21,22 +21,5 @@ Template.memberHomePage.helpers({
   },
   totalPoints: function() {
     return Meteor.users.totalPointsFor(Meteor.userId());
-  }
-});
-
-Template.memberHomePage.events({
-  'click #lnkTotalPoints': function(e) {
-    e.preventDefault();
-    Router.go('checkPoints');
-  },
-
-  'click #lnkEvents': function(e) {
-    e.preventDefault();
-    Router.go("eventsCalendar");
-  },
-
-  'click #lnkCheckIn': function(e) {
-    e.preventDefault();
-    Router.go('checkin');
   }
 });
