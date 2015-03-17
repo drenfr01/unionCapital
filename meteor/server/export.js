4fdede7dae20c1e6c320cfb873be95742dd02bb8
Tyler Sheffels <tsheffels@dimagi.com>
2015-03-16 21:38:33 -0400

    Add test for csv export, remove sample tests

--- meteor/server/export.js
+++ meteor/server/export.js
@@ -9,20 +9,17 @@ Meteor.methods({
     check(userId, String);
     var zip = new jsZip();
 
-    var getMembers = Meteor.users.find({roles: {$in: ["user"]}}, {
+    var members = Meteor.users.find({roles: {$in: ["user"]}}, {
       fields: {
         profile: 1
       }
     }).fetch();
 
-    var getMemberProfiles = _.map(getMembers, function(profile) {
-      return profile.profile;
-    });
-
+    var memberProfiles = helperFlattenProfile(members);
 
     var exportMembersAsCSV = function(done) {
       var csv = fastCsv;
-      csv.writeToString(getMemberProfiles, {
+      csv.writeToString(memberProfiles, {
         headers: true
       }, function(error, data) {
         if (error) {
@@ -33,12 +30,19 @@ Meteor.methods({
         done(error,null);
       });
     };
- 
+
     //Note: can also implement export as HTML, JSON, & XML
     var response = Async.runSync(function(done) {
-      exportMembersAsCSV(done);
+        exportMembersAsCSV(done);
     });
-    
+
+    // TODO, throw error if csv is empty so that we can catch if the csv export breaks
     return zip.generate({type: "base64"});
   }
 });
+
+helperFlattenProfile = function (members) {
+  return _.map(members, function(profile) {
+    return profile.profile;
+  });
+}
--- meteor/tests/mocha/server/serverTest.js
+++ meteor/tests/mocha/server/serverTest.js
@@ -1,9 +1,63 @@
 if (!(typeof MochaWeb === 'undefined')){
   MochaWeb.testOnly(function(){
-    describe("Server initialization", function(){
-      it("should have a Meteor version defined", function(){
-        chai.assert(Meteor.release);
+    describe("csv export", function(){
+      it("should should flatten the doc from mongo", function(){
+        query_return = [ { _id: 'v6g8xfHburkqMXkPe',
+            profile:
+             { firstName: 'Test2',
+               lastName: 'User2',
+               street1: '1 Rogers St',
+               street2: '',
+               city: 'Boston',
+               state: 'MA',
+               zip: '02141',
+               partnerOrg: 'Thrive in Five',
+               incomeBracket: '0-9,999',
+               numberOfKids: '1',
+               race: 'White' } },
+          { _id: 'MRycqYopjunL8kemC',
+            profile:
+             { firstName: 'Test',
+               lastName: 'User',
+               street1: '10 Emerson',
+               street2: '24H',
+               city: 'Boston',
+               state: 'MA',
+               zip: '02114',
+               partnerOrg: 'KIPP Academy',
+               incomeBracket: '25,000-29,999',
+               numberOfKids: '2',
+               race: 'African-American or Black' } } ]
+
+        outcome = helperFlattenProfile(query_return)
+
+        expected = [ { firstName: 'Test2',
+            lastName: 'User2',
+            street1: '1 Rogers St',
+            street2: '',
+            city: 'Boston',
+            state: 'MA',
+            zip: '02141',
+            partnerOrg: 'Thrive in Five',
+            incomeBracket: '0-9,999',
+            numberOfKids: '1',
+            race: 'White' },
+          { firstName: 'Test',
+            lastName: 'User',
+            street1: '10 Emerson',
+            street2: '24H',
+            city: 'Boston',
+            state: 'MA',
+            zip: '02114',
+            partnerOrg: 'KIPP Academy',
+            incomeBracket: '25,000-29,999',
+            numberOfKids: '2',
+            race: 'African-American or Black' } ]
+
+        chai.assert.deepEqual(outcome, expected);
       });
+
+
     });
   });
 }
