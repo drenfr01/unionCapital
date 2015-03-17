51b12d4a49930158dbc4bec278c8f4831188b353
drewproud <drew.proud@hotmail.com>
2015-03-17 00:28:44 -0400

    Added improved vagrant setup script

---
+++ vagrant/meteor_new.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install python-software-properties
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | sudo tee -a /etc/apt/sources.list.d/10gen.list
+sudo apt-get update
+sudo apt-get install -y git mongodb-10gen curl
+cd /usr/local
+wget http://nodejs.org/dist/v0.8.23/node-v0.8.23-linux-x86.tar.gz
+sudo tar -xvzf node-v0.8.23-linux-x86.tar.gz --strip=1
+rm -f node-v0.8.23-linux-x86.tar.gz
+curl https://install.meteor.com | sudo sh
+sudo cp -a /vagrant/. /home/vagrant/
+sudo mount --bind /home/vagrant/unionCapital/meteor/.meteor/ /vagrant/unionCapital/meteor/.meteor/
+echo "sudo mount --bind /home/vagrant/unionCapital/meteor/.meteor/ /vagrant/unionCapital/meteor/.meteor/" >> ~/.bashrc && source ~/.bashrc
+echo "Symlink created"
+sudo apt-get install vim
