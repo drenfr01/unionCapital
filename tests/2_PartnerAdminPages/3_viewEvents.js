3050a6fa0417fac8b7300f81e72edf9a16843407
drenfr01 <duncanrenfrow@gmail.com>
2015-03-25 11:05:05 -0400

    partway through adding test for partner events

---
+++ tests/2_PartnerAdminPages/3_viewEvents.js
@@ -0,0 +1,108 @@
+casper.test.comment("Testing Partner Admin Manage Events");
+
+casper.test.begin('Manage Partner Events', 34, function suite(test) {
+  casper.start(homeURL, function() {
+    casper.loginAsPartnerAdmin();
+  });
+  
+  //check home page main panel
+  casper.waitForSelector("#login-dropdown-list", function() {
+    test.assertExists('#manageEvents');
+    this.click('#manageEvents');
+  });
+
+  casper.waitWhileSelector('#manageEvents',function() {
+    test.assertExists('#addEvent');
+    test.assertExists('#search-box');
+    test.assertExists('#institutions');
+    test.assertExists('#categories');
+    //radio buttons
+    test.assertExists('#past');
+    test.assertExists('#current');
+
+    test.assertExists('#downloadCSV');
+    test.assertExists('table');
+    test.assertExists('.editEvent');
+    test.assertExists('.deleteEvent');
+
+    //default is current events
+    test.assertTextExists('Cambridge Science Festival'); //KIPP
+    test.assertTextDoesntExist('Cambridge Film Festival'); //Thrive in Five
+
+    this.click('#past');
+  });
+
+  //check past events
+  casper.wait(500, function() {
+    test.assertTextExists('Boston Music Festival'); //KIPP
+    test.assertTextDoesntExist('Somerville Cooking Festival'); //Thrive in Five
+    test.assertTextDoesntExist('Admin Add Points'); //Other
+
+    this.clickLabel('Somerville Cooking Festival','a');
+  });
+
+  //check click on single event
+  /*
+  casper.waitWhileSelector('#addEvent', function() {
+    test.assertExists('.back');
+    test.assertTextExists('A festival of cooking for the masses');
+    test.assertTextExists('50');
+    test.assertExists('iframe'); //TODO: this is a bad test for the google map
+
+    this.click('.back');
+  });
+
+  //got to Edit Event Page
+  casper.waitForSelector('#addEvent', function() {
+    this.page.injectJs('../../jquery-1.11.2.min.js');
+    this.evaluate(function() {
+      $("td:contains('Somerville Cooking Festival')").parent().find('.editEvent').click();
+    });
+  });
+  
+  //Check Page then edit event
+  casper.waitWhileSelector('#addEvent', function() {
+    test.assertExists('#eventName');
+    test.assertExists('#eventAddress');
+    test.assertExists('#eventUrl');
+    test.assertExists('#eventDesc');
+    test.assertExists('#eventOrg');
+    test.assertExists('#eventCategory');
+    test.assertExists('#eventDate');
+    test.assertExists("#eventPoints");
+    test.assertExists("#back");
+    test.assertExists('#submit');
+
+    this.sendKeys('#eventName', 'Somerville Cooking Jubilee', {reset: true});
+    this.click('#submit');
+  });
+
+  //Check event edited, then delete it
+  casper.wait(500, function(){
+    this.click('#past');
+    test.assertTextExists('Somerville Cooking Jubilee');  
+    test.assertTextExists('Boston Music Festival');
+
+    this.evaluate(function() {
+      $("td:contains('Somerville Cooking Jubilee')").parent().find('.deleteEvent').click();
+    });
+
+  });
+
+  casper.wait(500, function() {
+    test.assertTextDoesntExist('Somerville Cooking Jubilee');
+    test.assertTextExists('Boston Music Festival');
+    
+  });
+
+ */
+
+  casper.then(function() {
+    casper.logout(test);
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
