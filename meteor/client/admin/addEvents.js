990fce55f8c84aac4e5ad44d3c44e50eaf9a45d0
Tyler Sheffels <tyler.sheffels@gmail.com>
2015-03-29 15:50:16 -0400

    Removing addCommunityEvents and fixing whitespace...

--- meteor/client/admin/addEvents.js
+++ meteor/client/admin/addEvents.js
@@ -4,14 +4,14 @@ AutoForm.hooks({
       insert: function(doc, template) {
         doc.latitude = Session.get('latitude');
         doc.longitude = Session.get('longitude');
-        //UTC is not DST sensitive. So during Winter the US East Coast (EST) is 
+        //UTC is not DST sensitive. So during Winter the US East Coast (EST) is
         //5 hours behind UTC, but during summer it is 4 hours behind
         if(moment(doc.startDate).isDST()) {
           doc.startDate = moment(doc.startDate).subtract(1, 'hours').toDate();
           doc.endDate = moment(doc.endDate).subtract(1, 'hours').toDate();
         }
         return doc;
-      } 
+      }
     }
   }
 });
@@ -46,11 +46,11 @@ Template.addEvents.helpers({
 Template.addEvents.events({
   'click #geocodeButton': function(e) {
     e.preventDefault();
-    Meteor.call('geocodeAddress', $('#eventAddress').val(), 
+    Meteor.call('geocodeAddress', $('#eventAddress').val(),
                 function(error, result) {
                   if(error) {
                     addErrorMessage(error.reason);
-                    Router.go('addCommunityEvents');
+                    Router.go('addEvents');
                   } else {
                     Session.set('latitude', result.location.lat);
                     Session.set('longitude', result.location.lng);
