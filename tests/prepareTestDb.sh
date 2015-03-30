be8f6c2eb784617de6d775e4b98e822fec34ebd7
Drew Proud <drew.proud@hotmail.com>
2015-03-30 00:02:23 +0000

    Tests now pass

--- tests/prepareTestDb.sh
+++ tests/prepareTestDb.sh
@@ -1,1 +1,4 @@
-mongo localhost:3001/meteor --eval "db.users.remove({ 'profile.firstName': 'CasperJS' });"
+mongo localhost:3001/meteor --eval "
+db.users.remove({ 'profile.firstName': 'CasperJS' });
+db.users.remove({ 'emails.0.address': 'casperAdmin@gmail.com' });
+"
