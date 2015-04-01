ac8759dd95c47fb91ba93017599c926e4d4a0538
drenfr01 <duncanrenfrow@gmail.com>
2015-04-01 12:56:41 -0400

    denormalizing reservations and adding additional tests:

--- meteor/client/common/singleEvent.js
+++ meteor/client/common/singleEvent.js
@@ -2,15 +2,7 @@ Session.setDefault('rsvpList', null);
 
 Template.singleEvent.helpers({
   'rsvpList': function() {
-    var reservations = Reservations.find().fetch();
-    console.log(reservations);
-    return _.map(reservations, function(reservation) {
-      var user = Meteor.users.findOne({_id: reservation.userId});
-      console.log(user);
-      return {firstName: user.profile.firstName, 
-        lastName: user.profile.lastName.substring(0,1), 
-        numberOfPeople: reservation.numberOfPeople};
-    });
+    return Reservations.find();
   }
 });
 
