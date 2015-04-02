f07e43a51618f1e3d58299246b4d666921827bce
Tyler Sheffels <tyler.sheffels@gmail.com>
2015-04-01 21:43:47 -0400

    Rework fixtures, fix some bad transaction handling <-- this is worriesome

--- meteor/collections/users.js
+++ meteor/collections/users.js
@@ -1,6 +1,6 @@
 //needsApprovalFlag is boolean which determines if the transaction is pending or already submitted
 Meteor.users.transactionsFor = function(userId, needsApprovalFlag) {
-  //TODO: the check for pendingEventName is a complete hack. we need to have a way to logically separate 
+  //TODO: the check for pendingEventName is a complete hack. we need to have a way to logically separate
   //ad-hoc events from schedule ones, or better yet convert pending events to regular ones once they've been approved
   return Transactions.find(
     { userId: userId, needsApproval: needsApprovalFlag , deleteInd: { $ne: true }},
@@ -15,9 +15,9 @@ Meteor.users.totalPointsFor = function(userId) {
   .fetch()
   .reduce(function(sum, transaction) {
     var event = Transactions.eventFor(transaction);
-    if(event.isPointsPerHour) {
+    if(event && event.isPointsPerHour) {
       return Math.round(sum += event.pointsPerHour * transaction.hoursSpent);
-    } else {
+    } else if(event) {
       return sum += event.points;
     }
   }, 0);
