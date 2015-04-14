b50035a48f86b8be10842a6c182960d41427ff58
Drew Proud <drew.proud@hotmail.com>
2015-04-14 00:42:16 +0000

    Split out the event search so the check-in page only shows events 12 hours ahead or 12 hours behind

--- meteor/server/eventsSearch.js
+++ meteor/server/eventsSearch.js
@@ -1,25 +1,51 @@
 SearchSource.defineSource('eventsSearch', function(searchText, options) {
-    //I think we overwrite options because there is both a 
-    //server side and client side def 
-    //of this method, so it either calls the client side or 
-    //after the timeout
-    //does the server side 
-    var options = {
-      sort: {startDate: 1},
-      limit: 20
-    };
+  //I think we overwrite options because there is both a
+  //server side and client side def
+  //of this method, so it either calls the client side or
+  //after the timeout
+  //does the server side
+  var options = {
+    sort: {startDate: 1},
+    limit: 20
+  };
 
-    if(searchText && searchText.length > 0) {
-      var regExp = buildRegExp(searchText);
-      var selector = {$or: [
-        {name: regExp}, 
-        {description: regExp}
-      ]};
-      return Events.find(selector, options).fetch();
-    } else {
-      return Events.find({}).fetch();
-    }
-    
+  if( searchText && searchText.length > 0 ) {
+    var regExp = buildRegExp(searchText);
+    var selector = {
+      $or: [
+        { name: regExp },
+        { description: regExp }
+      ]
+    };
+    return Events.find(selector, options).fetch();
+  } else {
+    return Events.find({ eventDate: { $gte: startDate, $lte: endDate }}).fetch();
+  }
+});
+
+SearchSource.defineSource('checkinEventsSearch', function(searchText, options) {
+  var options = {
+    sort: {startDate: 1},
+    limit: 20
+  };
+
+  // Set the time interval
+  var startDate = moment().add(-12, 'h').toDate();
+  var endDate = moment().add(12, 'h').toDate();
+
+  if( searchText && searchText.length > 0 ) {
+    var regExp = buildRegExp(searchText);
+    var selector = {
+      $or: [
+        { name: regExp, eventDate: { $gte: startDate, $lte: endDate }},
+        { description: regExp, eventDate: { $gte: startDate, $lte: endDate }}
+      ]
+    };
+    return Events.find(selector, options).fetch();
+  } else {
+    return Events.find({ eventDate: { $gte: startDate, $lte: endDate }}).fetch();
+  }
+
 });
 
 function buildRegExp(searchText) {
