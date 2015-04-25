2cbd7c77757e5423bdf8edee84323dd7e674ac28
drenfr01 <duncanrenfrow@gmail.com>
2015-04-25 12:29:29 -0400

    SAVE POINT

--- meteor/client/common/collectUserDemographics.js
+++ meteor/client/common/collectUserDemographics.js
@@ -2,6 +2,19 @@
 FollowingOrganizations = new Meteor.Collection(null);
 
 Template.collectUserDemographics.rendered = function() {
+  // Define the object that directs the autofill targets
+  // Keys should match up with field IDs
+
+  var componentForm = {
+    street_number: 'short_name',
+    route: 'long_name',
+    locality: 'short_name',
+    administrative_area_level_1: 'short_name',
+    //country: 'long_name',
+    postal_code: 'short_name'
+  };
+
+  addressAutocomplete.initialize('inputAddress', componentForm);
 };
 
 Template.collectUserDemographics.helpers({
