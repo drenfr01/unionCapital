cd36e1b4762488dd8e0f372030ec33d8fed29ceb
Tyler Sheffels <tyler.sheffels@gmail.com>
2015-04-26 16:01:00 -0400

    Add delete indicator on user creation

--- meteor/server/userValidation.js
+++ meteor/server/userValidation.js
@@ -1,12 +1,4 @@
-/*
-Accounts.validateNewUser(function(user) {
-  //this grabs the email address out of the user email
-  var emails = _.pluck(_.flatten(_.pluck(Meteor.users.find().fetch(),
-        'emails')),'address');
-  if(user.email in emails) {
-    return false;
-  } else {
-    return true;
-  }
-});
-*/
+Accounts.onCreateUser(function (options, user) {
+  user.deleteInd = false
+  return user
+})
