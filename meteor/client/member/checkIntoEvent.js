6bfa4ea7e1a6ba07ec3c34c6ab87da36378cfd56
Drew Proud <drew.proud@hotmail.com>
2015-04-28 00:47:41 +0000

    Added radio buttons to check in page to select between current and past events

--- meteor/client/member/checkIntoEvent.js
+++ meteor/client/member/checkIntoEvent.js
@@ -10,7 +10,7 @@ CheckinEventsSearch = new SearchSource('checkinEventsSearch', fields, options);
 
 // Gets the data for use in the getEvents helper
 function getEventsData() {
-var events = CheckinEventsSearch.getData({
+  var events = CheckinEventsSearch.getData({
     sort: {eventDate: 1}
   });
 
@@ -25,7 +25,7 @@ var events = CheckinEventsSearch.getData({
     var minStartDate = new Date();
     var maxEndDate = new Date();
 
-    if (Session.equals('eventTimeframe', 'today')) {
+    if (Session.equals('eventTimeframe', 'current')) {
 
       // Use only events defined as today for a current check in
       var minStartDate = moment().add(AppConfig.checkIn.today.hoursBehind, 'h').toDate();
@@ -100,7 +100,7 @@ Template.checkIntoEvent.rendered = function() {
 
   // We don't want to start out with an event selected
   Session.set('selectedEvent', null);
-  Session.set('eventTimeframe', 'today');
+  Session.set('eventTimeframe', 'current');
   setToggleValues();
 
   // Populate the event list on load with no filters
@@ -111,7 +111,6 @@ Template.checkIntoEvent.rendered = function() {
 Template.checkIntoEvent.helpers({
 
   'getEvents': function() {
-
     var eventsArray = getEventsData();
     setMapMarkers(eventsArray);
 
@@ -159,6 +158,11 @@ Template.checkIntoEvent.events({
   'click #clearBtn': function() {
     CheckinEventsSearch.search('');
     $('#eventSearchBox').val('');
+  },
+
+  'click #pastOrCurrentRdoDiv': function(e) {
+    var thisValue = $('.radio-button input[type=radio]:checked').val();
+    Session.set('eventTimeframe', thisValue);
   }
 
   // 'click #cancel': function(e) {
