4f2bdf2f33ee04c5c7c5b891cf0307e5d378581f
Drew Proud <drew.proud@hotmail.com>
2015-04-29 02:03:57 +0000

    Save point

--- meteor/client/member/memberProfile.js
+++ meteor/client/member/memberProfile.js
@@ -1,14 +1,39 @@
 Template.memberProfile.helpers({
+
   memberProfile: function() {
     return Meteor.user().profile;
   },
+
   memberEmail: function() {
     return Meteor.user().emails[0].address;
+  },
+
+  settingPassword: function() {
+    return Accounts._loginButtonsSession.get('inChangePasswordFlow');
   }
 });
 
 Template.memberProfile.events({
   'click #edit': function() {
     Router.go('editMemberProfile');
+  },
+
+  'click #change-pw': function(event) {
+    event.stopPropagation();
+    Accounts._loginButtonsSession.resetMessages();
+    Accounts._loginButtonsSession.set('inChangePasswordFlow', true);
+    Meteor.flush();
+  },
+
+  'click #sign-out': function(event) {
+    Meteor.logout(function(error) {
+      if (typeof accountsUIBootstrap3.logoutCallback === 'function') {
+        accountsUIBootstrap3.logoutCallback(error);
+      }
+    });
   }
 });
+
+Template.memberProfile.rendered = function() {
+  Accounts._loginButtonsSession.set('inChangePasswordFlow', false);
+}
