a90ccfe0915ac675a95effe1805128507a319c9f
Tyler Sheffels <tyler.sheffels@gmail.com>
2015-05-02 17:32:53 -0400

    Adding comment

--- meteor/server/userValidation.js
+++ meteor/server/userValidation.js
@@ -1,5 +1,6 @@
 Accounts.onCreateUser(function (options, user) {
   user.deleteInd = false
+  //Setting profile is necessary here even though meteor claims that createUser should insert it
   user.profile = options.profile
   return user
 })
