559e5e5d783838ee71136040eb262d0501a5df55
drenfr01 <duncanrenfrow@gmail.com>
2015-05-04 21:46:40 -0400

    image viewer search box and routing complete

--- meteor/client/admin/superAdmin/imageViewer.js
+++ meteor/client/admin/superAdmin/imageViewer.js
@@ -1,18 +1,35 @@
+Template.imageViewer.rendered = function() {
+  Session.set('searchString', null);
+};
+
 Template.imageViewer.helpers({
   'images': function() {
-    var selector = Session.get('selector');
-    if(Session.get('selector')) {
-      selector = Session.get('selector');
-    }
     var userImages = Images.find().fetch();
-    return _.map(userImages, function(userImage) {
+    //keeping reactivity by using function
+    var searchString = function() { return Session.get('searchString'); }();
+    var users = _.map(userImages, function(userImage) {
       var user = Meteor.users.findOne(userImage.metadata.userId);
       _.extend(user, {
-        imageId: userImage._id
+        imageId: userImage._id,
+        submissionTime: userImage.metadata.submissionTime
       });
-      return user;
-    });
+      //
+      if(_.isString(searchString)) {
+        if (user.profile.firstName.indexOf(searchString) > -1  ||
+            user.profile.lastName.indexOf(searchString) > -1 || 
+              user.profile.partnerOrg.indexOf(searchString) > -1
+           ) {
+             return user; //user matches search box string
+           } else {
+             return; //user does not match search box string
+           }
+      } else {  //nothing in search box, return all users
+        return user;
+      }
 
+    });
+    users = _.filter(users, function(user) { return _.isObject(user);});
+    return _.isEmpty(users) ? null : users;
   },
   'imageUrl': function(imageId) {
     if(Images.findOne(imageId)) {
@@ -27,5 +44,8 @@ Template.imageViewer.helpers({
 Template.imageViewer.events({
   'click .showImage': function(e) {
     Session.set('modalDataContext', this);
+  },
+  'keyup #search-box': function(e) {
+    Session.set('searchString', $('#search-box').val());
   }
 });
