e801515495b32b88a2594ccf984636ae18cae3d5
drenfr01 <duncanrenfrow@gmail.com>
2015-05-12 22:50:22 -0400

    users signing up with facebook now will have their names and email pulled over

--- meteor/client/common/createNewUser.js
+++ meteor/client/common/createNewUser.js
@@ -1,3 +1,17 @@
+Template.createNewUser.helpers({
+  //For users signing up with FB,
+  //this pulls in their information
+  'firstName': function() {
+    return  Meteor.user() ? Meteor.user().services.facebook.first_name : null;
+  },
+  'lastName': function() {
+    return  Meteor.user() ? Meteor.user().services.facebook.last_name : null;
+  },
+  'email': function() {
+    return Meteor.user() ? Meteor.user().services.facebook.email : null;
+  }
+});
+
 Template.createNewUser.events({
   'click #back': function(e) {
     e.preventDefault();
