9ea4874efa66562aa6fe335154cd1e7151179e23
drenfr01 <duncanrenfrow@gmail.com>
2015-05-28 20:39:20 -0400

    removing commented out block of code

--- meteor/server/helpers.js
+++ meteor/server/helpers.js
@@ -6,26 +6,6 @@ function toRadians(x) {
      return x * (Math.PI / 180);
 }
 
-// haversineFormula = function(event, userLng, userLat) {
-//     var eventLat = event.latitude;
-//     var eventLong = event.longitude;
-
-//     //Haversine formula, source: http://www.movable-type.co.uk/scripts/latlong.html
-//     var R = 6371; // km
-//     var userLatRadians = toRadians(userLat);
-//     var eventLatRadians = toRadians(eventLat);
-//     var deltaLatRadians = toRadians(eventLat-userLat);
-//     var deltaLongRadians = toRadians(eventLong-userLng);
-
-//     var a = Math.sin(deltaLatRadians/2) * Math.sin(deltaLatRadians/2) +
-//       Math.cos(userLatRadians) * Math.cos(eventLatRadians) *
-//       Math.sin(deltaLongRadians/2) * Math.sin(deltaLongRadians/2);
-//     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-//     var distance = R * c;
-
-//     return distance;
-// };
-
 emailHelper = function(to, from, subject, text) {
       Email.send({
          to: to,
