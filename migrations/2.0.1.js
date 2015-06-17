abc35c790f0bc330674e998741cba10b8582f187
drenfr01 <duncanrenfrow@gmail.com>
2015-06-17 11:36:22 -0400

    adding in migration script for ucb button

---
+++ migrations/2.0.1.js
@@ -0,0 +1,15 @@
+Events.insert({
+        name: AppConfig.ucbButtonEvent, //UCB Button
+        address: 'Boston, MA',
+        url: 'unioncapitalboston.com',
+        description: 'The UCB member wore a UCB button to an event',
+        active: 0,
+        eventDate: new Date(),
+        duration: 0,
+        institution: "Other",
+        category: "Other",
+        isPointsPerHour: false,
+        points: 50,
+        adHoc: true,
+        deleteInd: true
+});
