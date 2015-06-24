5558c0e711e724da1203651d8cdbf73b3cab1746
drenfr01 <duncanrenfrow@gmail.com>
2015-06-23 23:47:02 -0400

    updating migration with prod statement

--- migrations/2.0.1.js
+++ migrations/2.0.1.js
@@ -14,3 +14,21 @@ Events.insert({
         adHoc: true,
         deleteInd: true
 });
+//Prod insert statement - 06/23/2015
+db.events.insert({
+  _id: 'EJQMDdJQC3Xxfu9xK',
+  name: 'UCB Button',
+  address: 'Boston, MA',
+  url: 'unioncapitalboston.com',
+  description: 'The UCB member wore a UCB button to an event',
+  active: 0,
+  eventDate: new Date(),
+  duration: 0,
+  institution: "Other",
+  category: "Other",
+  isPointsPerHour: false,
+  points: 50,
+  adHoc: true,
+  deleteInd: true
+
+});
