eef0ca8348ed7359d560377e2b0f0db8b1feb0e5
Drew Proud <drew.proud@hotmail.com>
2016-04-23 16:03:03 -0400

    Users are now prevented from seeing the login page if already logged in #361 #351

--- meteor/client/common/landing.js
+++ meteor/client/common/landing.js
@@ -1,15 +1,10 @@
-Template.landing.rendered = function() {
-  //TODO: this may inadvertently bounce an admin user
-  //to the member home page...
-  if (Meteor.userId()) {
-    Router.go('memberHomePage');
-  }
-};
+/* global addErrorMessage */
+/* global Roles */
 
 Template.landing.helpers({
   getData: function() {
     return {template: 'createNewUser'};
-  }
+  },
 });
 
 Template.landing.events({
@@ -30,7 +25,7 @@ Template.landing.events({
     });
   },
 
-  'click #loginSubmit': function(e) {
+  'submit #loginForm': function(e) {
     e.preventDefault();
 
     $('#loginForm').validate({
@@ -42,33 +37,33 @@ Template.landing.events({
       rules: {
         userEmail: {
           required: true,
-          email: true
+          email: true,
         },
         userPassword: {
-          required: true
-        }
-      }
+          required: true,
+        },
+      },
     });
     var isValid = $('#loginForm').valid();
 
-    if(isValid) {
-      var email =  $('#userEmail').val().toLowerCase();
-      var password = $('#userPassword').val();
-
-      Meteor.loginWithPassword(email, password, function(error) {
-        if(error) {
-          addErrorMessage(error.reason);
-        } else {
-          if(Roles.userIsInRole(Meteor.userId(), 'admin')) {
-            Router.go('adminHomePage');
-          } else if (Roles.userIsInRole(Meteor.userId(), 'partnerAdmin')){
-            Router.go('partnerAdminHomePage');
-          } else {
-            Router.go('memberHomePage');
-          }
-        }
-      });
-
+    if(!isValid) {
+      addErrorMessage('Invalid entry');
     }
+
+    var email =  $('#userEmail').val().toLowerCase();
+    var password = $('#userPassword').val();
+
+    Meteor.loginWithPassword(email, password, function(error) {
+      if(error) {
+        addErrorMessage(error.reason);
+      } else {
+        if(Roles.userIsInRole(Meteor.userId(), 'admin')) {
+          return Router.go('adminHomePage');
+        } else if (Roles.userIsInRole(Meteor.userId(), 'partnerAdmin')){
+          return Router.go('partnerAdminHomePage');
+        }
+        return Router.go('memberHomePage');
+      }
+    });
   }
 });
