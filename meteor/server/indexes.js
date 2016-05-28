1ec7d523d154d4285f3f33076b0b8bdf4ee07f1e
Duncan Renfrow-Symon <duncanrenfrow@gmail.com>
2016-05-28 16:26:01 -0400

    ensuring proper indexing

--- meteor/server/indexes.js
+++ meteor/server/indexes.js
@@ -4,11 +4,17 @@ Meteor.startup(function() {
   Transactions._ensureIndex({ "userId": 1, "transactionDate": -1});
   Transactions._ensureIndex({ "eventId": 1  });
   Transactions._ensureIndex({"transactionDate": -1});
+  Transactions._ensureIndex({'approved': 1, 'eventId': 1});
+
   Meteor.users._ensureIndex({"profile.points": 1});
+
   Events._ensureIndex({"name": 1})
   Events._ensureIndex({"institution": 1});
   Events._ensureIndex({"category": 1});
-  Events._ensureIndex({"eventDate": 1});
+  Events._ensureIndex({"eventDate": 1, 'adHoc': 1});
+
+  PartnerOrgs._ensureIndex({'name': 1});
+  Feedback._ensureIndex({'eventInstitution': 1 });
 
   console.log("Ending indexing");
 });
