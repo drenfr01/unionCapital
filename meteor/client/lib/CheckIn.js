a4de3e5a003e00ec14e3ba4419b89e24498e7b62
Drew Proud <drew.proud@hotmail.com>
2016-05-30 19:05:44 -0400

    Updated checkin and publications

--- meteor/client/lib/CheckIn.js
+++ meteor/client/lib/CheckIn.js
@@ -1,3 +1,6 @@
+/* global CheckIn:true */
+/* global UserPhoto */
+/* global ReactiveVar */
 
 CheckIn = function(defaultHours) {
   var self = this;
@@ -5,7 +8,6 @@ CheckIn = function(defaultHours) {
   self.hours = new ReactiveVar(defaultHours);
   self.checkingIn = new ReactiveVar(false);
   self.userPhoto = new UserPhoto();
-  self.attributes = {};
   self.eventName = null;
   self.eventDescription = null;
   self.eventDate = null;
@@ -13,69 +15,66 @@ CheckIn = function(defaultHours) {
 
   // Calls insertTransaction and routes the user
   // Private function
-  function callInsert(callback) {
-    Meteor.call( 'insertTransaction', self.attributes, callback );
+  function callInsert(attributes, callback) {
+    Meteor.call( 'insertTransaction', attributes, callback );
   }
 
   // Sets the attributes prior to calling the insert function
   // Semiprivate function - should not be called directly
-  self.insertTransaction = function(eventId, addons, imageId, callback) {
+  self._insertTransaction = function(eventId, addons, imageId, callback) {
 
     try {
-
-      var parsedHours = parseFloat(self.hours.get());
-
-      self.attributes = {
+      const attributes = {
         userId: Meteor.userId(),
-        hoursSpent: parsedHours ? parsedHours : 0
+        hoursSpent: parseFloat(self.hours.get()) || 0,
       };
 
       // If new, then don't set the eventId to avoid check() errors
       if (eventId === 'new' && self.eventName && self.eventDescription && self.eventDate) {
-        self.attributes.eventName = self.eventName;
-        self.attributes.eventDescription = self.eventDescription;
-        self.attributes.eventDate = self.eventDate;
-        self.attributes.category = self.category;
+        attributes.eventName = self.eventName;
+        attributes.eventDescription = self.eventDescription;
+        attributes.eventDate = self.eventDate;
+        attributes.category = self.category;
       } else if (eventId && eventId !== 'new') {
         // Else set the event ID
-        self.attributes.eventId = eventId;
+        attributes.eventId = eventId;
       } else {
         throw new Meteor.Error('NO_EVENT_NAME', 'Please fill out all fields');
       }
 
       // Instead of just passing a null imageId field, this omits the field
       // entirely to stay consistent with the check() function called on the server
-      if( imageId )
-        self.attributes.imageId = imageId;
+      if (imageId) {
+        attributes.imageId = imageId;
+      }
 
       //omits the field entirely, same as above comment
       //TODO: make this a check for empty
       if (!R.isEmpty(addons)) {
-        self.attributes.addons = addons;
+        attributes.addons = addons;
       }
 
       // If lat or lng is null, then try to get it one more time
       // Useful if the user accessed this page from a link or bookmark
       if (gmaps.currentLocation.lat && gmaps.currentLocation.lng) {
 
-        self.attributes.userLat = gmaps.currentLocation.lat;
-        self.attributes.userLng = gmaps.currentLocation.lng;
-        callInsert(callback);
+        attributes.userLat = gmaps.currentLocation.lat;
+        attributes.userLng = gmaps.currentLocation.lng;
+        callInsert(attributes, callback);
 
       } else {
 
         gmaps.getCurrentLocation(function(error, currentLocation) {
 
           if (!error) {
-            self.attributes.userLat = currentLocation.lat;
-            self.attributes.userLng = currentLocation.lng;
+            attributes.userLat = currentLocation.lat;
+            attributes.userLng = currentLocation.lng;
           }
 
-          callInsert(callback);
+          callInsert(attributes, callback);
         });
       }
-    }
-    catch(error) {
+    } catch(error) {
       // Just pass it through if it is a meteor error
       if (error.errorType === 'Meteor.Error') {
         callback(error);
@@ -100,7 +99,7 @@ CheckIn.prototype.submitCheckIn = function(eventId, addons, callback) {
   self.checkingIn.set(true);
 
   // This makes sure checkingIn never stays true on error
-  function newCallback(error, result) {
+  function wrappedCallback(error, result) {
     self.checkingIn.set(false);
     callback( error, result );
   }
@@ -109,13 +108,13 @@ CheckIn.prototype.submitCheckIn = function(eventId, addons, callback) {
   if( self.userPhoto && self.userPhoto.photoURI.get() ) {
     self.userPhoto.insert(function(err, fileObj) {
       if ( err ) {
-        newCallback(err, null);
+        wrappedCallback(err, null);
       } else {
-        self.insertTransaction(eventId, fileObj._id, newCallback);
+        self._insertTransaction(eventId, addons, fileObj._id, wrappedCallback);
       }
     });
   } else {
-    self.insertTransaction(eventId, addons, null, newCallback);
+    self._insertTransaction(eventId, addons, null, wrappedCallback);
   }
 };
 
