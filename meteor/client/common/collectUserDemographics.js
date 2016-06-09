d35d73ef930a87c3168d4f8a7463e8000cb5dd05
Duncan Renfrow-Symon <duncanrenfrow@gmail.com>
2016-06-09 13:04:10 -0400

    removing unnecessary information

--- meteor/client/common/collectUserDemographics.js
+++ meteor/client/common/collectUserDemographics.js
@@ -1,15 +1,11 @@
 //Used to hold organizations user wants to follow
-FollowingOrganizations = new Meteor.Collection(null);
 PartnerOrganizations = new Meteor.Collection(null);
 
 Template.collectUserDemographics.onCreated(function() {
   this.subscribe('eventCategories');
   this.subscribe('eventOrgs');
   this.subscribe('partnerOrganizations');
-  this.subscribe('kids');
   this.subscribe('races');
-  this.subscribe('ucbappaccess');
-  this.subscribe('numberOfPeople');
   this.subscribe('partnerOrgSectors');
 });
 
@@ -29,12 +25,6 @@ Template.collectUserDemographics.helpers({
   partnerOrgs: function(e) {
     return PartnerOrganizations.find(); 
   },
-  followingOrgs: function() {
-    return FollowingOrganizations.find();
-  },
-  UCBAppAccess: function() {
-    return UCBAppAccess.find();
-  }
 });
 
 Template.collectUserDemographics.events({
@@ -47,18 +37,11 @@ Template.collectUserDemographics.events({
     PartnerOrganizations.remove(this._id); 
   },
 
-  'change #followingOrgs': function(e) {
-    FollowingOrganizations.upsert({description: e.target.value}, {description: e.target.value, 
-        name: $("#followingOrgs option:selected").text()
-    });
-  },
   'click #back': function(e) {
     e.preventDefault();
     Session.set('signupPage', 'createNewUser');
   },
-  'click .removeOrg': function(e) {
-    FollowingOrganizations.remove(this._id);
-  },
+
   'click #next': function(e) {
     e.preventDefault();
 
@@ -68,12 +51,12 @@ Template.collectUserDemographics.events({
     userAttributes.profile.city = $('#locality').val();
     userAttributes.profile.state = $('#administrative_area_level_1').val();
     userAttributes.profile.zip = $('#postal_code').val();
-    userAttributes.profile.partnerOrg = R.pluck('name', PartnerOrganizations.find().fetch());
+    userAttributes.profile.partnerOrg = 
+      R.pluck('name', PartnerOrganizations.find().fetch());
     userAttributes.profile.race = $("#races").val();
     userAttributes.profile.role = 'user';
-    userAttributes.profile.followingOrgs = FollowingOrganizations.find().fetch();
-    userAttributes.profile.gender = $("#genderForm input[type='radio']:checked").val();
-    userAttributes.profile.UCBAppAccess = $('#device').val();
+    userAttributes.profile.gender = 
+      $("#genderForm input[type='radio']:checked").val();
 
     $("#userAddressForm").validate({
       highlight: function(element, errorClass) {
