9953093165e974eff02ad8e5caece6803e9f9824
Nick <ncreel@marlboro.edu>
2018-04-04 12:20:20 -0400

    working on i18n todo list, commiting to check for file differences

---
+++ meteor/server/migrations/defaultDisplayLanguage.js
@@ -0,0 +1,12 @@
+//var changeLanguage = function () {
+//  Meteor.users.update({$set: {displayLanguage: AppConfig.languages.english}},{multi:true});
+//}
+//var backwards = function(){
+//  console.log('not sure what a down for this would be...seems we have nowhere to go but up!')
+//}
+//remember $multi (what was this again?)
+//Migrations.add({
+//  version: 1,
+//  name: 'Change displayLanguage of all users to English',
+//  up: changeLanguage();
+//});
